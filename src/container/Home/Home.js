ef2c2e3460dd6496d2118c887a1970119ed4a1e8
fanliang <15702455610@163.com>
2019-05-07 20:05:20 +0800

    2

---
+++ src/container/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Home extends React.Component{
+    render(){
+        return (
+            <div style={{width: '100%',height: 200}}>
+                <span style={{fontWeight:'bold',fontSize:20}}>my blog</span>
+            </div>
+        )
+    }
+}
+export default Home;
