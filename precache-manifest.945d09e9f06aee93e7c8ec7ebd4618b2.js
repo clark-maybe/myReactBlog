91238179231b1f763224a2fa08a4d9f70e2371f7
fanliang <15702455610@163.com>
2019-05-08 19:47:06 +0800

    Updates

---
+++ precache-manifest.945d09e9f06aee93e7c8ec7ebd4618b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6a99afc7afa4ab5376fe954ea3e54b",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "baba8a305f23c27a761c",
+    "url": "/my-react-app/static/css/2.4358233e.chunk.css"
+  },
+  {
+    "revision": "42547f67be43ede8e709",
+    "url": "/my-react-app/static/css/main.fd4d78ba.chunk.css"
+  },
+  {
+    "revision": "baba8a305f23c27a761c",
+    "url": "/my-react-app/static/js/2.affb0d92.chunk.js"
+  },
+  {
+    "revision": "42547f67be43ede8e709",
+    "url": "/my-react-app/static/js/main.4aa6cc82.chunk.js"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  },
+  {
+    "revision": "790b1975c24324d93ec41360df9c7806",
+    "url": "/my-react-app/static/media/banner1.790b1975.jpg"
+  },
+  {
+    "revision": "57c4fb016e9d59636516d8cbccce9d07",
+    "url": "/my-react-app/static/media/banner2.57c4fb01.jpg"
+  },
+  {
+    "revision": "87b16c21e98629910b935c371f24348b",
+    "url": "/my-react-app/static/media/banner3.87b16c21.jpg"
+  },
+  {
+    "revision": "bb4ea06484b54be139551a6acc4baace",
+    "url": "/my-react-app/static/media/logo.bb4ea064.jpg"
+  }
+]);
