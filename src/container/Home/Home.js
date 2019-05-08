59c025df43036bc2eeea21e78e429151da250756
fanliang <15702455610@163.com>
2019-05-08 16:17:29 +0800

    2

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,12 +1,76 @@
 import React from 'react'
+import {Layout, Menu, Breadcrumb, Icon} from 'antd'
+import Logo from '../../../src/img/logo.jpg'
 
-class Home extends React.Component{
-    render(){
+const {SubMenu} = Menu;
+const {Header, Content, Footer, Sider} = Layout;
+
+class Home extends React.Component {
+    render() {
         return (
-            <div style={{width: '100%',height: 200}}>
-                <span style={{fontWeight:'bold',fontSize:20}}>my blog</span>
+            <div className={'homeStyle'}>
+                <Layout>
+                    <Header className="header">
+                        <div className="logo"
+                             style={{display: 'inline-block', boxShadow: '2px 2px 2px', marginRight: '20px'}}>
+                            <img src={Logo} alt="logo" style={{width: '54px'}}/>
+                        </div>
+                        <Menu
+                            theme="dark"
+                            mode="horizontal"
+                            defaultSelectedKeys={['home']}
+                            style={{lineHeight: '64px', display: 'inline-block'}}
+                        >
+                            <Menu.Item key="home">Home</Menu.Item>
+                            <Menu.Item key="aboutMe">aboutMe</Menu.Item>
+                        </Menu>
+                    </Header>
+                    <Content style={{padding: '0 50px', minHeight: '800px'}}>
+                        {/*<Breadcrumb style={{margin: '16px 0'}}>*/}
+                            {/*<Breadcrumb.Item>Home</Breadcrumb.Item>*/}
+                            {/*<Breadcrumb.Item>List</Breadcrumb.Item>*/}
+                            {/*<Breadcrumb.Item>App</Breadcrumb.Item>*/}
+                        {/*</Breadcrumb>*/}
+                        <Layout style={{padding: '24px 0', background: '#fff', marginTop: '30px'}}>
+                            <Sider width={200} style={{background: '#3da8ff'}}>
+                                <Menu
+                                    mode="inline"
+                                    defaultSelectedKeys={['1']}
+                                    defaultOpenKeys={['sub1']}
+                                    style={{height: '100%'}}
+                                >
+                                    <SubMenu key="sub1" title={<span><Icon type="user"/>subnav 1</span>}>
+                                        <Menu.Item key="1">option1</Menu.Item>
+                                        <Menu.Item key="2">option2</Menu.Item>
+                                        <Menu.Item key="3">option3</Menu.Item>
+                                        <Menu.Item key="4">option4</Menu.Item>
+                                    </SubMenu>
+                                    <SubMenu key="sub2" title={<span><Icon type="laptop"/>subnav 2</span>}>
+                                        <Menu.Item key="5">option5</Menu.Item>
+                                        <Menu.Item key="6">option6</Menu.Item>
+                                        <Menu.Item key="7">option7</Menu.Item>
+                                        <Menu.Item key="8">option8</Menu.Item>
+                                    </SubMenu>
+                                    <SubMenu key="sub3" title={<span><Icon type="notification"/>subnav 3</span>}>
+                                        <Menu.Item key="9">option9</Menu.Item>
+                                        <Menu.Item key="10">option10</Menu.Item>
+                                        <Menu.Item key="11">option11</Menu.Item>
+                                        <Menu.Item key="12">option12</Menu.Item>
+                                    </SubMenu>
+                                </Menu>
+                            </Sider>
+                            <Content style={{padding: '0 24px', minHeight: 280}}>
+                                Content
+                            </Content>
+                        </Layout>
+                    </Content>
+                    <Footer style={{textAlign: 'center'}}>
+                        Created by <span style={{fontWeight: 'bold', fontSize: '15px'}}>Clark</span> with React
+                    </Footer>
+                </Layout>
             </div>
         )
     }
 }
+
 export default Home;
