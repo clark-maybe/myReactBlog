0795f691f7d5d44e46032d701907f5d811cdb907
fanliang <15702455610@163.com>
2019-05-09 15:16:19 +0800

    2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import {Route, Router} from 'react-router-dom'
-import history from './history'
 import Home from './container/Home/Home'
 
 class App extends React.Component {
